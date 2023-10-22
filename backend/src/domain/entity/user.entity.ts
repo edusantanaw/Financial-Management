93b8c6eb79a8528b39793d1899440ac7d013e8fb
edusantanaw <techabmax@gmail.com>
2023-10-22 20:47:13 -0300

    feat: create user entity

---
+++ backend/src/domain/entity/user.entity.ts
@@ -0,0 +1,43 @@
+import { randomUUID } from "node:crypto";
+
+interface IUser {
+  id?: string;
+  name: string;
+  cpfCnpj: string;
+  email: string;
+  password: string;
+  token?: string;
+}
+
+export class UserEntity {
+  protected id: string;
+  protected name: string;
+  protected cpfCnpj: string;
+  protected email: string;
+  protected password: string;
+  protected token?: string;
+
+  constructor(data: IUser) {
+    this.id = data.id ?? randomUUID();
+    this.name = data.name;
+    this.cpfCnpj = data.cpfCnpj;
+    this.email = data.email;
+    this.password = data.password;
+    this.token = data.token;
+  }
+
+  public get getUserEntity(): IUser {
+    return {
+      id: this.id,
+      cpfCnpj: this.cpfCnpj,
+      email: this.email,
+      name: this.name,
+      password: this.password,
+      token: this.token,
+    };
+  }
+
+  public set setToken(token: string) {
+    this.token = token;
+  }
+}
